d9bc33b054fe33c60d4cb34793c43db262e4ee78
dyun0726 <90127420+dyun0726@users.noreply.github.com>
2024-06-24 18:48:28 +0900

    test character
    테스트 캐릭터 이동 및 애니메이션 구현성

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 3f;
+    public float jumpForce = 5f;
+    public float xInput;
+    public bool isGround;
+
+    private Rigidbody2D playerRb;
+    private SpriteRenderer spriteRenderer;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        
+    }
+
+    private void FixedUpdate() {
+        xInput =Input.GetAxis("Horizontal");
+        float jumpInput = Input.GetAxisRaw("Jump");
+
+        if (isGround && jumpInput != 0){
+            playerRb.velocity = new Vector2(xInput * speed, playerRb.velocity.y + jumpInput * jumpForce);
+            anim.SetBool("Jumping", true);
+        } else {
+            playerRb.velocity = new Vector2(xInput * speed, playerRb.velocity.y);
+        }
+        
+    }
+
+    private void LateUpdate() {
+        anim.SetFloat("Speed", Mathf.Abs(xInput));
+        if (xInput != 0){
+            spriteRenderer.flipX = xInput < 0;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.CompareTag("Ground")){
+            isGround = true;
+            anim.SetBool("Jumping", false);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other) {
+        if (other.gameObject.CompareTag("Ground")){
+            isGround = false;
+        } 
+    }
+}
