5a7ce0718b5de9bc37ac2e2f02b3dfff415e5d82
CHICHIT <33713817+CHICHIT@users.noreply.github.com>
2024-07-05 18:05:52 +0900

    강화요소 추가

---
+++ Assets/scripts/Upgrades/GroundUpgradeData.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GroundUpgrade", menuName = "Scriptable Object/Upgrade/Ground")]
+
+public class GroundUpgradeData : BaseUpgradeData
+{
+    public enum GroundUpgradeType { maxhealth, stance,resurrection, superstance, shield, damagereduce}
+    public GroundUpgradeType groundUpgradeType;
+
+    public override void ApplyUpgrade(int level)
+    {
+        switch (groundUpgradeType)
+        {
+            case GroundUpgradeType.maxhealth:
+                Debug.Log("Not Implemented");
+                break;
+            case GroundUpgradeType.stance:
+                Debug.Log("Not Implemented");
+                break;
+            case GroundUpgradeType.resurrection:
+                Debug.Log("Not Implemented");
+                break;
+            case GroundUpgradeType.superstance:
+                Debug.Log("Not Implemented");
+                break;
+            case GroundUpgradeType.shield:
+                Debug.Log("Not Implemented");
+                break;
+            case GroundUpgradeType.damagereduce:
+                Debug.Log("Not Implemented");
+                break;
+
+        }
+    }
+}
