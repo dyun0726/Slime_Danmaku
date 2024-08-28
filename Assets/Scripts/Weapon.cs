a0c8671f1aa3d3eddcd30971e8e6e368514e4af3
dyun0726 <90127420+dyun0726@users.noreply.github.com>
2024-08-28 16:57:46 +0900

    근접 무기 버그 수정

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -15,12 +15,37 @@ public class Weapon : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other) {
         if (weaponAttack != null){
             if (weaponAttack.canDealDamage){
-                if (other.gameObject.layer == 9){
+                if (other.gameObject.layer == 9 || other.gameObject.layer == 12){
                     Enemy enemy = other.GetComponent<Enemy>();
 
-                    if (enemy != null){
-                        enemy.TakeDamage(PlayerManager.Instance.strength, PlayerManager.Instance.armorPt, PlayerManager.Instance.armorPtPercent, true);
-                    } else {
+                    if (enemy != null)
+                    {
+                        bool isCritical = Random.value < (PlayerManager.Instance.crirate / 100f);
+                        float nDamage = PlayerManager.Instance.strength;
+                        if (isCritical)
+                        {
+                            Debug.Log("critical!");
+                            nDamage *= 1 + PlayerManager.Instance.criticalDamage / 100f;
+                        }
+                        enemy.TakeDamage(nDamage, PlayerManager.Instance.armorPt, PlayerManager.Instance.armorPtPercent, true);
+
+                        if (PlayerManager.Instance.dotDamge > 0)
+                        { // dot damge가 활성화되어있으면
+                            enemy.SetDotDamage(PlayerManager.Instance.dotDamge);
+                        }
+
+                        if (PlayerManager.Instance.atkReduction > 0)
+                        { // 공격 감소 디버프 탄막이면
+                            enemy.SetAttackReduce(3f, PlayerManager.Instance.atkReduction);
+                        }
+
+                        if (PlayerManager.Instance.lifeSteel > 0)
+                        {
+                            PlayerManager.Instance.Heal(PlayerManager.Instance.lifeSteel);
+                        }
+                    } 
+                    else
+                    {
                         Debug.Log("Fail to find Emeny component");
                     }
                     
